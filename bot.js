5b9d36c09e6b45ee720d2494e15250cb888b819c
Pomdre <bataldensondre@gmail.com>
2020-04-06 14:15:06 +0200

    new vg api

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
-const request = require("request")
+const request = require("request");
+const country = require('countryjs');
 
 //Setings
 const prefix = '!';
@@ -27,31 +28,6 @@ client.on('ready', () => {
   }, 15e3);
 });
 
-//Todays date
-function today() {
-  var today = new Date(); 
-  var dd = today.getDate(); 
-  var mm = today.getMonth()+1; 
-  //January is 0! 
-  var yyyy = today.getFullYear(); 
-  if(dd<10){dd='0'+dd} 
-  if(mm<10){mm='0'+mm} 
-  return yyyy+'-'+mm+'-'+dd;
-}
-
-//Yesterday
-function yesterday() {
-    //Todays date
-    var today = new Date(); 
-    var dd = today.getDate()-1; 
-    var mm = today.getMonth()+1; 
-    //January is 0! 
-    var yyyy = today.getFullYear(); 
-    if(dd<10){dd='0'+dd} 
-    if(mm<10){mm='0'+mm} 
-    return yyyy+'-'+mm+'-'+dd;
-}
-
 //Just don't know why but i added a ping option
 client.on('message', msg => {
     if (msg.content.toLowerCase() === corona + " ping" || msg.content.toLowerCase() === korona + " ping" || msg.content.toLowerCase() === covid + " ping") {
@@ -61,7 +37,7 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === corona || msg.content.toLowerCase() === korona || msg.content.toLowerCase() === covid || msg.content.toLowerCase() === corona + " vg" || msg.content.toLowerCase() === korona + " vg" || msg.content.toLowerCase() === covid + " vg") {
-    var url = "https://www.vg.no/spesial/2020/corona-viruset/data/norway-region-data/"
+    var url = "https://redutv-api.vg.no/corona/v1/sheets/norway-table-overview?region=county"
     request({
       url: url,
       json: true
@@ -77,13 +53,12 @@ function (error, response, body) {
           "title": "Korona/Covid-19 statistikk i Norge\nData fra vg:",
           "color": 0x66c255,
           "description": 
-          "\n**Smittede i Norge totalt:**\n" + obj.metadata.confirmed.total + " eller " + ((obj.metadata.confirmed.total/obj.metadata.population)*100).toFixed(2) + "%" +
-          "\n**Nye tilfeller i dag:**\n" + obj.metadata.confirmed.newToday + 
-          "\n**Bekreftete tilfeller per tusen innbygger:**\n" + obj.metadata.confirmed.per1kCapita + 
-          "\n**I dag / i går totalt:**\n" + obj.timeseries.total.confirmed[today()] + "/" + obj.timeseries.total.confirmed[yesterday()] +
-          "\n**Nye tilfeller i dag vs i går:**\n" + obj.metadata.confirmed.newToday + "/" + obj.metadata.confirmed.newYesterday +
-          "\n**Døde i Norge totalt:**\n" + obj.metadata.dead.total + " eller " + ((obj.metadata.dead.total/obj.metadata.population)*100).toFixed(5) + "%" +
-          "\n**Døde i dag / i går:**\n" + obj.metadata.dead.newToday + "/" + obj.metadata.dead.newYesterday +
+          "\n**Smittede i Norge totalt:**\n" + obj.totals.confirmed + " eller " + ((obj.totals.confirmed/country.population('NO'))*100).toFixed(2) + "%" +
+          "\n**Nye tilfeller i dag:**\n" + obj.totals.changes.newToday + 
+          "\n**Nye tilfeller i dag vs i går:**\n" + obj.totals.changes.newToday + "/" + obj.totals.changes.newYesterday +
+          "\n**Bekreftete tilfeller per tusen innbygger:**\n" + (obj.totals.confirmed/country.population('NO')*1000).toFixed(2) + 
+          "\n**Døde i Norge totalt:**\n" + obj.totals.dead + " eller " + ((obj.totals.dead/country.population('NO'))*100).toFixed(8) + "%" +
+          "\n**Døde i dag / i går:**\n" + obj.totals.changes.deathsToday + "/" + obj.totals.changes.deathsYesterday +
           "\n**FHI sier det er flere smittede i Norge enn tallene viser. Mørketallene kan være store, fordi mange ikke testes.**\n",
           "footer": {
           "icon_url": "https://www.nextcloud.pomdre.net/index.php/apps/files_sharing/publicpreview/CAzpPzLHsrY2fxP?x=3840&y=1422&a=true&file=Pomdre%2520logo%2520Brukes%2520n%25C3%25A5.png&scalingup=0",
